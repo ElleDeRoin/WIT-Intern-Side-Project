c12e8ebfab46f21100b0f4346c87e58d35901f49
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2022-07-08 12:58:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main
 {
     public static void main(String[] args)
     {
-        //TicTacToe game = new TicTacToe(1, 3); //1 player, 3 rounds
-        //game.play();
+        TicTacToe game = new TicTacToe(1, 3); //1 player, 3 rounds
+        game.play();
     }
 }
